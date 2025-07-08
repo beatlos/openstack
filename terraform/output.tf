4b6ee03e63a7af433701183fb63400f65c594154
beatlos <70963118+beatlos@users.noreply.github.com>
2025-07-08 20:27:18 +0200

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,7 @@
 output "public_ips" {
-  value = hcloud_server.servers
+  description = "Public IPv4 addresses of all servers"
+  value = {
+    for name, server in hcloud_server.servers :
+    name => server.ipv4_address
+  }
 }
